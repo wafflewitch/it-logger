ae8277b7e94a3e9573549898393c0480cb63f34b
wafflewitch <jessicapruitt717@gmail.com>
2020-07-07 12:02:21 -0500

    In App component, import useEffect hook and use it to invoke M.AutoInit to initialize Materialize JS. In index.html, remove unnecessary comments, change title, and add stylesheet for Material Icons.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
 const App = () => {
+  useEffect(() => {
+    // Init Materialilze JS
+    M.AutoInit();
+  });
+
   return <div className='App'>IT Logger</div>;
 };
 
